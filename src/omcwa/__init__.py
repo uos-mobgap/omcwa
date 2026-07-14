5274ee766508a9c6f0beeec56284f7178690d7c5
Artem Vakhutinskiy <vahut@yandex.ru>
2026-07-14 16:24:25 +0100

    initial scaffold for omcwa package and build tooling

---
+++ src/omcwa/__init__.py
@@ -0,0 +1,39 @@
+"""omcwa: CWA load, calibrate, and resample."""
+
+__version__ = "0.1.0"
+
+from omcwa.calibrate import OmConvertCalibrate
+from omcwa.defaults import (
+    DEFAULT_CALIBRATE,
+    DEFAULT_INTERPOLATE,
+    DEFAULT_SAMPLE_RATE_HZ,
+    DEFAULT_STATIONARY_TIME,
+    USE_FILE_SAMPLE_RATE,
+    InterpolateMode,
+)
+from omcwa.process import load_cwa, process_cwa
+from omcwa.resample import OmConvertResample
+from omcwa.types import (
+    CalibratedRecording,
+    Calibration,
+    ProcessedRecording,
+    RawRecording,
+)
+
+__all__ = [
+    "__version__",
+    "load_cwa",
+    "process_cwa",
+    "OmConvertCalibrate",
+    "OmConvertResample",
+    "Calibration",
+    "RawRecording",
+    "CalibratedRecording",
+    "ProcessedRecording",
+    "InterpolateMode",
+    "DEFAULT_SAMPLE_RATE_HZ",
+    "DEFAULT_INTERPOLATE",
+    "DEFAULT_STATIONARY_TIME",
+    "DEFAULT_CALIBRATE",
+    "USE_FILE_SAMPLE_RATE",
+]
